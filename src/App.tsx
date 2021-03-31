3f4f484dcdb6eb5471ffede749b71bb173682a0f
Liz Drumm <lizdrumm@gmail.com>
2021-03-31 15:38:17 -0500

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// import {TextField} from "./TextField"
+import {Counter} from "./Counter"
+
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Counter>
+        {({count, setCount}) => (
+          <div>
+            {count}
+            <button onClick = {() => setCount(count + 1)}>+ </button>
+          </div>
+        )}
+      </Counter>
+    </div>
+  );
+}
+
+export default App;
